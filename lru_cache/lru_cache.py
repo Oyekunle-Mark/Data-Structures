2ba4704c5f4e8905f33f594fea605aa982046fa2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-15 00:41:11 +0100

    First pass  implementation of the lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,16 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        # state the limit of node the lru cache can hold
+        self.limit = limit
+        # track the current number of node it is holding
+        self.node_count = 0
+        # create a linked list to hold the key value pair
+        self.cache_list = DoublyLinkedList()
+        # create a storage dict for fast acccess to node in the cache
+        self.cache_storage = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +27,17 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        # if the key does not exist in the cache_storage return None
+        if key not in self.cache_storage:
+            return None
+        # otherwise return the value associated with the key and move the node to the head of the cache_list
+        else:
+            node = self.cache_storage[key]["node"]
+            self.cache_list.move_to_front(node)
+
+            return self.cache_storage[key]["value"]
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +49,24 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        # if the key exist in the cache_storage, replace its value and move the node to the head in the cache_list
+        if key in self.cache_storage:
+            self.cache_storage[key]["value"] = value
+            key_node = self.cache_storage[key]["node"]
+            self.cache_list.move_to_front(key_node)
+        # otherwise, check if the length of the node is up to the limit
+        else:
+            if self.node_count == self.limit:
+                # if it is, delete the tail of the cache_list and remove the key value pair from the cache_storage
+                popped_key = self.cache_list.remove_from_tail()
+                self.cache_storage.pop(popped_key)
+                # add the new key value pair to the cache_storage and cache_list head
+                node = self.cache_list.add_to_head(key)
+                self.cache_storage[key] = {"value": value, "node": node}
+            # if the length is not up to the limit, simply add the new key value pair to the cache_storage and cache_list head and increment node_count
+            else:
+                node = self.cache_list.add_to_head(key)
+                self.cache_storage[key] = {"value": value, "node": node}
+                self.node_count += 1
