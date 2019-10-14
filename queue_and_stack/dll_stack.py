650788dc26da02252e8f621307f43f7353478dd1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-14 14:22:57 +0100

    Implementerd the stack data structure

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,16 +1,31 @@
 from doubly_linked_list import DoublyLinkedList
 
+
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
+        # set the length to zero
+        self.length = 0
 
     def push(self, value):
-        pass
+        # use the add_to_head method on the DoublyLinkedList instance passing the value
+        self.storage.add_to_head(value)
+        # increment the length
+        self.length += 1
 
     def pop(self):
-        pass
+        # if length is zero return none
+        if not self.length:
+            return None
+
+        # use the remove_from_head method on the DoublyLinkedList instance
+        popped_value = self.storage.remove_from_head()
+        # decrement the length
+        self.length -= 1
+        # return the value popped
+        return popped_value
 
     def len(self):
-        pass
+        return self.length
