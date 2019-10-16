eeb90d34fbe953dd6b951555ff340dc720fb50a5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-16 16:09:21 +0100

    Finished get max

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -21,17 +21,17 @@ class BinarySearchTree:
                 self.left = BinarySearchTree(value)
                 return
             # otherwise
-            elif not self.left.right:
+            elif not self.left.right and self.left.value < value:
                 self.left.right = BinarySearchTree(value)
                 return
 
-            
             if value < self.left.value:
                 self.left = self.left.left
                 self.insert(value)
-            else:
-                self.left.right = self.left.right
-                self.insert(value)
+            # else:
+            #     self.left.right = self.left.right
+            #     self.insert(value)
+
                 # repeat process for left
         # RIGHT CASE
         # check if the new nodes value is greater than or equal to the current parent value
@@ -45,64 +45,171 @@ class BinarySearchTree:
                 self.right = BinarySearchTree(value)
                 return
             # otherwise
-            elif not self.right.left:
+            elif not self.right.left and self.right.value > value:
                 self.right.left = BinarySearchTree(value)
                 return
 
-            
-            if value < self.right.value:
+            if value > self.right.value:
                 self.right = self.right.right
                 self.insert(value)
-            else:
-                self.right.left = self.right.left
-                self.insert(value)
+            # else:
+            #     self.right.left = self.right.left
+            #     self.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
+
     def contains(self, target):
+        if target == self.value or target == self.left.value or target == self.right.value:
+            return True
+
         # BASE CASE
+        elif self.left and not self.right:
+            return False
 
         # LEFT CASE
+        if target < self.value:
+            # if the is no left child,
+            if not self.left:
+                return False
+            # otherwise
+            elif not self.left.right:
+                return False
 
-        # RIGHT CASE
-        pass
+            if value < self.left.value:
+                self.left = self.left.left
+                self.contains(value)
+            # else:
+            #     # self.left.right = self.left.right
+            #     # self.contains(value)
+            #     pass
+
+        else:
+            # if the is no left child,
+            if not self.right:
+                return False
+            # otherwise
+            elif not self.right.left:
+                return False
+
+            if value > self.right.value:
+                self.right = self.right.right
+                self.contains(value)
+            # else:
+            #     # self.right.left = self.right.left
+            #     # self.contains(value)
+            #     pass
 
     # Return the maximum value found in the tree
+
     def get_max(self):
         # BASE CASE
         # if empty tree
+        if self.right is None:
+            return self.value
             # return none
 
         # RECURSIVE
         # if the the is no right value
+        if self.right.right is None:
+            return self.right.value
+
             # return the root value
         # return the get max of the the right node
+        self.right = self.right.right
+        self.get_max()
 
         # ITTERATIVE
         # set a max value variable to keep track of max value
         # get a ref to current node
         # check if we are at a valid tree node
-            # if our current value is greater than the max value
-                # update the max value
-            # move on to the next right node in the tree
-            # setting the current node to the current nodes right
+        # if our current value is greater than the max value
+        # update the max value
+        # move on to the next right node in the tree
+        # setting the current node to the current nodes right
         # return our max value
         pass
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
+        cb(self.value)
         # base case
 
         # left case
 
         # right case
-        pass
+
+        # if target == self.value or target == self.left.value or target == self.right.value:
+        #     return True
+
+        # BASE CASE
+        if self.left is None and self.right is None:
+            return
+
+        if self.left:
+            cb(self.left.value)
+
+            # if self.left:
+            if self.left.left:
+                cb(self.left.left.value)
+                self.left = self.left.left
+            if self.left.right:
+                cb(self.left.right.value)
+                self.left.right = self.left.right.right
+
+
+        if self.right:
+            cb(self.right.value)
+
+            # if self.right:
+            if self.right.right:
+                cb(self.right.right.value)
+                self.right = self.right.right
+            if self.right.left:
+                cb(self.right.left.value)
+                self.right.left = self.right.left.left
+
+        
+
+        # LEFT CASE
+        # if target < self.value:
+        #     # if the is no left child,
+        #     if not self.left:
+        #         return False
+        #     # otherwise
+        #     elif not self.left.right:
+        #         return False
+
+        #     if value < self.left.value:
+        #         self.left = self.left.left
+        #         self.contains(value)
+        #     # else:
+        #     #     # self.left.right = self.left.right
+        #     #     # self.contains(value)
+        #     #     pass
+
+        # else:
+        #     # if the is no left child,
+        #     if not self.right:
+        #         return False
+        #     # otherwise
+        #     elif not self.right.left:
+        #         return False
+
+        #     if value > self.right.value:
+        #         self.right = self.right.right
+        #         self.contains(value)
+        #     # else:
+        #     #     # self.right.left = self.right.left
+        #     #     # self.contains(value)
+        #     #     pass
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+
     def in_order_print(self, node):
         pass
 
@@ -130,7 +237,7 @@ class BinarySearchTree:
 
 # bst = BinarySearchTree(5)
 
-    
+
 # bst.insert(2)
 # bst.insert(3)
 # # bst.insert(7)
